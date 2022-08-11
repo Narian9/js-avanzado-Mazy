1a5460d248f3d08242c1913f5c9c7104f0f5afd0
Daniella <dansmc.courses@gmail.com>
2022-08-11 12:27:55 -0500

    clase 2

---
+++ 1-node/1.1-funciones-base/global/index.js
@@ -0,0 +1,3 @@
+console.log(this);
+console.log('Ruta donde esta mi archivo', __dirname);
+console.log('Ruta + nombre de mi archivo', __filename);
---
+++ 1-node/1.1-funciones-base/os/index.js
@@ -0,0 +1,2 @@
+const os = require('os');
+console.log('version del sistema:  ', os.version());
